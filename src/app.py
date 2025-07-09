f3dfafa342702bf1ee0e9c18c7e6a72658f092e1
Mariam Elkamhawy <mariam.kamhawy@silverkeytech.com>
2025-07-09 18:58:31 +0300

    fixing syntax error

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ def info():
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
         'message': 'You are doing great, little human! <3',
-        'deployed_on': 'kubernetes'
+        'deployed_on': 'kubernetes',
         'env': 'dev',
         'app_env': 'testing'
     })
